394591aaff4a90a1faffac9713b0fb1cd2a543c8
kakum3 <104210758+kakum3@users.noreply.github.com>
2023-01-24 16:28:44 +0100

    Ejercicio-2

---
+++ Ejercicio02/index.js
@@ -0,0 +1,13 @@
+const lista = [
+   "Jesus",
+   46 ,
+ true,
+new Date(1976,10, 24),
+{
+  titulo: "Angeles y Demonios",
+  autor: "Dan Brown",
+  fecha: new Date(2017, 7, 29),
+  url:"https://www.amazon.es/%C3%81ngeles-demonios-Bestseller-Dan-Brown/dp/840809923X"
+},
+];
+console.log(lista);
