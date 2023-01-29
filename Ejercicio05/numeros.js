6b19e0f5114f8b592df2b0d02ee4bce273df552f
kakum3 <104210758+kakum3@users.noreply.github.com>
2023-01-29 09:17:43 +0100

    ejercicio5

---
+++ Ejercicio05/numeros.js
@@ -0,0 +1,15 @@
+let alturaCm = 179
+let alturaM = 1.79
+
+let pesoKg = 80.5
+
+let alturaRed = Math.ceil(alturaM)
+console.log(alturaRed);
+ 
+
+let pesoRed = Math.floor(pesoKg)
+console.log(pesoRed);
+
+
+const iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+console.log(iguales);
