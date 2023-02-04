182d3beccb9374c01741f4794d52a03c252aea3a
kakum3 <104210758+kakum3@users.noreply.github.com>
2023-02-04 10:00:57 +0100

    Ejercicio-7

---
+++ Ejercicio07/conjuntos.js
@@ -0,0 +1,12 @@
+
+
+const array = ["Facundo", "Angela", "Jesús", "María", "Rafa"]
+
+const miSet = new Set(array)
+console.log(miSet);
+
+miSet.add("Jesús")
+console.log(miSet);
+
+miSet.add("JavaScript")
+console.log(miSet);
