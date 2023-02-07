f968cf60cecd1b0d6a5c179eafeec171217e7314
kakum3 <104210758+kakum3@users.noreply.github.com>
2023-02-07 07:38:30 +0100

    ejercicio08

---
+++ Ejercicio08/funciones.js
@@ -0,0 +1,16 @@
+
+function allTrue() {
+  return true
+  
+}
+
+async function miPromesa(){
+  return setTimeout(() => console.log("Hola soy una promesa"), 5000)
+}
+
+function pares (){
+  let id = 0
+  while(true){
+    yield += 2
+  }
+}
